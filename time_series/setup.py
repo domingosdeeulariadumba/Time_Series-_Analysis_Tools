fc2fa8ee5f08d8e74371b3206ae9a4788d33fdfc
Domingos de Eulária Dumba <110714056+domingosdeeulariadumba@users.noreply.github.com>
2024-11-22 13:05:55 +0100

    Update setup.py

--- time_series/setup.py
+++ time_series/setup.py
@@ -5,14 +5,13 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'numpy>=1.21.0',    # Specify version if necessary
+        'numpy>=1.21.0',
         'pandas>=1.3.0',
         'scipy>=1.7.0',
         'matplotlib>=3.4.0',
         'statsmodels>=0.12.0'
-        # Add other dependencies as needed
     ],
-    # Optional metadata
+    
     author='Domingos de Eulária Dumba',
     author_email='domingosdeeulariadumba@gmail.com',
     description = 'A package for time series analysis',
